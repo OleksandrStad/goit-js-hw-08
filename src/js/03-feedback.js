6bd22cf443ef2553c25f396039d5a3161a0dd3f0
OleksandrStad <mmk.polymer@gmail.com>
2023-01-25 20:54:18 +0200

    add Ex3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,10 +2,48 @@ import throttle from 'lodash.throttle';
 
 const inputEmailEl = document.querySelector('input');
 const textareaMessageEl = document.querySelector('textarea');
+const actionFormsEl = document.querySelector('.feedback-form');
+const keyLocalStorage = "feedback-form-state";
 
-console.log(inputEmailEl);
-console.log(textareaMessageEl);
+// console.log(inputEmailEl);
+// console.log(textareaMessageEl);
+// console.log(actionFormsEl);
 
+inputEmailEl.addEventListener('input', throttle(setLocalStorage, 500));
+textareaMessageEl.addEventListener('input', throttle(setLocalStorage, 500));
 
+actionFormsEl.addEventListener('click', localStorageObj);
 
-console.log('hello word')
+function localStorageObj(e) {
+    e.preventDefault();
+    if (!inputEmailEl.value || !textareaMessageEl.value) {
+        return;
+    }
+
+    localStorage.removeItem(keyLocalStorage);
+
+    console.log({
+        email: inputEmailEl.value,
+        message: textareaMessageEl.value,
+    });
+
+    inputEmailEl.value = '';
+    textareaMessageEl.value = '';
+};
+
+function setLocalStorage() {
+    localStorage.setItem(
+        keyLocalStorage,
+        JSON.stringify({
+            email: inputEmailEl.value,
+            message: textareaMessageEl.value,
+        })
+    )
+};
+
+const savedState = localStorage.getItem(keyLocalStorage);
+if (savedState) {
+    const state = JSON.parse(savedState);
+    inputEmailEl.value = state.email;
+    textareaMessageEl.value = state.message;
+}
